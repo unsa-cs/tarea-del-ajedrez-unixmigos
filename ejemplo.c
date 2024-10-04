af2401156f16bf0171b309bdc337dc4d6ae1549f
jparedesma <jparedesma@unsa.edu.pe>
2024-10-03 23:49:26 -0500

    Probamos la funcion repeatV con 4 piezas que se generaron verticalmente

--- ejemplo.c
+++ ejemplo.c
@@ -5,5 +5,5 @@ void display() {
   char **white_square = whiteSquare;
   char **black_square = reverse(whiteSquare);
   char **black_queen = reverse(queen);
-  interpreter(repeatH(join(white_square, black_queen), 6));
+  interpreter(repeatV(join(white_square, black_queen), 4));
 }
