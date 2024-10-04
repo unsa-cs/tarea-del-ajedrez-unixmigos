db047e78bb9ac217a37411804ea2e97e781a1ebd
kenny <kborjav@unsa.edu.pe>
2024-10-03 22:31:11 -0500

    Se probo la rotacion con un join, hizo una rotacion como si fuera una sola pieza

--- ejemplo.c
+++ ejemplo.c
@@ -2,8 +2,8 @@
 #include "figures.h"
 
 void display() {
-  char **blackKnight = rotateL(reverse(knight));
-  char **whiteKnight = rotateR(rotateR(rotateR(rotateR(knight))));
-  char **Rook_Knight = join(blackKnight, whiteKnight); 
+  char **blackKnight = reverse(knight);
+  char **whiteKnight = knight;
+  char **Rook_Knight = rotateR(join(blackKnight, whiteKnight)); 
   interpreter(Rook_Knight);
 }
