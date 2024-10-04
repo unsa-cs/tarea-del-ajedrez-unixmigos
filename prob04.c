fd6a5f19fdbd1145cd501046ed84370bced00aaa
kenny <kborjav@unsa.edu.pe>
2024-10-04 18:11:28 -0500

    Usamos la funcion superImpose con una fila completa de cuadrados(row_first) y con una ficha(white_rook), suponemos que solo muestra la interseccion de ambas.

--- prob04.c
+++ prob04.c
@@ -5,5 +5,8 @@ void display() {
   char **white_square = whiteSquare;
   char **black_square = reverse(whiteSquare);
   char **row_first = repeatH(join(black_square, white_square), 4);
-  interpreter(row_first);
+  char **white_rook = rook;
+  char **impose_row_rook = superImpose(white_rook, row_first);
+
+  interpreter(impose_row_rook);
 }
