c3769e3f928422873fb664efabeb852b6a500159
JoseAle123 <jmachacamu@unsa.edu.pe>
2024-10-04 17:16:03 -0500

    Utilizando la funcion repeatV para repetir 2 veces la figura (fila doble), se obtuvo la imagen propuesta.

--- prob03.c
+++ prob03.c
@@ -6,5 +6,5 @@ void display() {
   char **black_Square = reverse(whiteSquare);
   char **chessFila = repeatH(join(white_Square, black_Square), 4);
   char **chessFila2 = up(reverse(chessFila), chessFila);  
-  interpreter(chessFila2);
+  interpreter(repeatV(chessFila2, 2));
 }
