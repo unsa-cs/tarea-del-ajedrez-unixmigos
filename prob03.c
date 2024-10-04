a6c44f51a113cea00dd6ad5e14083380fdb5e9ee
JoseAle123 <jmachacamu@unsa.edu.pe>
2024-10-04 17:03:24 -0500

    Se uso reverse en la primera fila (el primer argumento de up),  para que este bien intercalado con la fila 2.

--- prob03.c
+++ prob03.c
@@ -5,6 +5,6 @@ void display() {
   char **white_Square = whiteSquare;
   char **black_Square = reverse(whiteSquare);
   char **chessFila = repeatH(join(white_Square, black_Square), 4);
-  char **chessFila2 = up(chessFila, chessFila);
+  char **chessFila2 = up(reverse(chessFila), chessFila);  
   interpreter(chessFila2);
 }
