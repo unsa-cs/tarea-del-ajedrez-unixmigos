8307c093facac1aa691adf9916eedc87494267cf
kenny <kborjav@unsa.edu.pe>
2024-10-04 18:25:06 -0500

    Verificamos nuestra hipotesis agregando dos fichas mas y uniendolas, luego se uso superImpose mostrando solo la interseccion entre la fila y las piezas.

--- prob04.c
+++ prob04.c
@@ -6,7 +6,10 @@ void display() {
   char **black_square = reverse(whiteSquare);
   char **row_first = repeatH(join(black_square, white_square), 4);
   char **white_rook = rook;
-  char **impose_row_rook = superImpose(white_rook, row_first);
+  char **white_knight = knight;
+  char **white_bishop = bishop;
+  char **row_pieces = join(join(white_rook, white_knight), white_bishop);
+  char **impose_row_pieces = superImpose(row_pieces, row_first);
 
-  interpreter(impose_row_rook);
+  interpreter(impose_row_pieces);
 }
