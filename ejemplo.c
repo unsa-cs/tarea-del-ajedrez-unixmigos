8cf0b72033ad13c974c546ace214b74bd35785ca
Jesus <jchancayauris@unsa.edu.pe>
2024-10-03 22:59:01 -0500

    Cambiamos el orden de los argumentos enviados a superImpose. Notamos que el primer argumento sobrepone al segundo argumento.

--- ejemplo.c
+++ ejemplo.c
@@ -5,6 +5,6 @@ void display() {
   char **white_square = whiteSquare;
   char **black_square = reverse(whiteSquare);
   char **white_queen = queen;
-   
-  interpreter(superImpose(black_square, white_queen));
+  
+  interpreter(superImpose(white_queen, black_square));
 }
