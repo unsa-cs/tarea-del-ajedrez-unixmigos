a9867eb436149ecb7b5101e36ed77f8a316fa7aa
rventurave <rventurave@unsa.edu.pe>
2024-10-04 16:25:50 -0500

    Con la funcion flipV creamos el espejo vertical de la figura de la mitad de la fila,Luego usando 2 veces la funcion rotateL, rotamos la figura de manera hoaria 180 grados, y se obtuvo el objetivo.

--- prob02.c
+++ prob02.c
@@ -5,7 +5,7 @@ void display() {
   char **white_Square = whiteSquare;
   char **black_Square = reverse(whiteSquare);
   char **chessFila = repeatH(join(black_Square, whiteSquare), 2);
-  char **tmp = flipV(chessFila);
+  char **tmp = rotateL(rotateL((flipV(chessFila))));
   char **tmp2 = join(chessFila, tmp);
   interpreter(tmp2);
 }
