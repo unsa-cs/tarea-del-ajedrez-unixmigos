1c71b3b456e88d250d29be43fb31f0e68a0180a3
jparedesma <jparedesma@unsa.edu.pe>
2024-10-04 23:39:30 -0500

    Probamos crear una nueva variable con el caballo rotado antes de aplicar las funciones de flipH y rotateL(utilizadas anteriormente) de esta forma evitamos que el caballo apunte a otra direccion.

--- prob04.c
+++ prob04.c
@@ -11,8 +11,9 @@ void display() {
   char **white_king = king;
   char **white_queen = queen;
   char **row_pieces_left = join(join(white_rook, white_knight), white_bishop);
+  char **row_pieces_left_knight = join(join(white_rook, flipV(white_knight)), white_bishop);
   char **row_pieces_center = join(white_queen, white_king);
-  char **row_pieces_right = flipV(flipH(rotateL(rotateL(row_pieces_left))));
+  char **row_pieces_right = flipH(rotateL(rotateL(row_pieces_left_knight)));
   char **row_pieces = join(join(row_pieces_left, row_pieces_center), row_pieces_right); 
   char **impose_row_pieces = superImpose(row_pieces, row_first);
 
