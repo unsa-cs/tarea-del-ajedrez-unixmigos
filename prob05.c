05c53891ab615e245c396c693636c9d32de7f26f
rventurave <rventurave@unsa.edu.pe>
2024-10-05 20:35:45 -0500

    Se hizo la inicializacion de la variable identificada por pieces asignandole 0, pero nos sigue saliendo el mismo error(SIGSEGV)

--- prob05.c
+++ prob05.c
@@ -1,15 +1,16 @@
 #include "chess.h"
 #include "figures.h"
+#include <threads.h>
 
 void display() {
   char **white_Square = whiteSquare;
   char **black_Square = reverse(whiteSquare);
   char **Row_First = repeatH(join(black_Square, white_Square), 4);
-  char **pieces;
+  char **pieces = 0;
 
   for (int i = 0; i < 8; i++) {
-    if (i == 0 || i == 7)
-      pieces = join(pieces, reverse(rook));
+    if (i == 7)
+      pieces = join(rook, pieces);
   }
   interpreter(superImpose(pieces, Row_First));
 }
