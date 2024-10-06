a2cf2a2a9aecea5c10d0a6b2c1516b1b03ba2c89
JoseAle123 <jmachacamu@unsa.edu.pe>
2024-10-05 21:30:41 -0500

    Eliminamos los reverse que usamos en cada condicional y en la inicializacion, y se uso en la funcion interpreter

--- prob05.c
+++ prob05.c
@@ -6,19 +6,19 @@ void display() {
   char **white_Square = whiteSquare;
   char **black_Square = reverse(whiteSquare);
   char **Row_First = repeatH(join(black_Square, white_Square), 4);
-  char **pieces = reverse(rook);
+  char **pieces = rook;
 
   for (int i = 1; i < 8; i++) {
     if (i == 7)
-      pieces = join(pieces, reverse(rook));
+      pieces = join(pieces, rook);
     else if (i == 1 || i == 6)
-      pieces = join(pieces, reverse(knight));
+      pieces = join(pieces, knight);
     else if (i == 2 || i == 5)
-      pieces = join(pieces, reverse(bishop));
+      pieces = join(pieces, bishop);
     else if (i == 3)
-      pieces = join(pieces, reverse(queen));
+      pieces = join(pieces, queen);
     else
-      pieces = join(pieces, reverse(king));
+      pieces = join(pieces, king);
   }
-  interpreter(superImpose(pieces, Row_First));
+  interpreter(superImpose(reverse(pieces), Row_First));
 }
