a6f2d1a2538aa299d71387ae20b1d349de8902d1
kenny <kborjav@unsa.edu.pe>
2024-10-05 21:54:03 -0500

    Utilizando la solucion del ejercicio 5 ahora se añadio un fila de peones unsando repeatH.

---
+++ prob06.c
@@ -0,0 +1,26 @@
+#include "chess.h"
+#include "figures.h"
+#include <threads.h>
+
+void display() {
+  char **white_Square = whiteSquare;
+  char **black_Square = reverse(whiteSquare);
+  char **Row_First = repeatH(join(black_Square, white_Square), 4);
+  char **pieces = rook;
+
+  for (int i = 1; i < 8; i++) {
+    if (i == 7)
+      pieces = join(pieces, rook);
+    else if (i == 1 || i == 6)
+      pieces = join(pieces, knight);
+    else if (i == 2 || i == 5)
+      pieces = join(pieces, bishop);
+    else if (i == 3)
+      pieces = join(pieces, queen);
+    else
+      pieces = join(pieces, king);
+  }
+  
+  char **white_row_pawn = repeatH(pawn, 8);
+  interpreter(white_row_pawn);
+}
