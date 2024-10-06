0c4fe2fc27eae07e893b950243882823d77bfa4d
rventurave <rventurave@unsa.edu.pe>
2024-10-05 20:51:41 -0500

    Probamos inicializando pieces con rook, que es un arreglo. Obtuvimos la visualizacion de las piezas, en el commit anterior suponiamos que al asignarle 0 era como inicializar una pieza vacia.

--- prob05.c
+++ prob05.c
@@ -6,11 +6,11 @@ void display() {
   char **white_Square = whiteSquare;
   char **black_Square = reverse(whiteSquare);
   char **Row_First = repeatH(join(black_Square, white_Square), 4);
-  char **pieces = 0;
+  char **pieces = reverse(rook);
 
   for (int i = 0; i < 8; i++) {
     if (i == 7)
-      pieces = join(rook, pieces);
+      pieces = join(pieces, reverse(rook));
   }
   interpreter(superImpose(pieces, Row_First));
 }
