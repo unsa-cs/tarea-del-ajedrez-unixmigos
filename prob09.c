51997848bf288b18551f24dc8ccae2e5d5b800eb
kenny <kborjav@unsa.edu.pe>
2024-10-06 15:42:09 -0500

    Se añadio dos condicionales para el primer cuadrado cuando hay una ficha o no sobre esta, el resultado no fue el esperado.

--- prob09.c
+++ prob09.c
@@ -36,8 +36,24 @@ void display() {
   char **pieces[] = {pawn, rook, knight, bishop, queen, king, reverse(pawn), reverse(rook), reverse(knight), reverse(bishop), reverse(queen), reverse(king)};
 
   for (int i = 1; i <= 8; i++) {
-    char **tem_row = pawn;
+    char **tem_row;
     for (int j = 1; j <= 8; j++) {
+      if (j == 1 && matriz[i - 1][j - 1] == 0 && (i % 2 != 0)) {
+        tem_row = black_square;
+        continue;
+      }
+      else if (j == 1 && matriz[i - 1][j - 1] == 0 )
+        tem_row = whiteSquare;
+      else {
+        if (i % 2 != 0){
+          tem_row = black_square;
+          tem_row = superImpose(pieces[matriz[i - 1][j - 1] - 1], tem_row);
+        } 
+        else{
+          tem_row = whiteSquare;
+          tem_row = superImpose(pieces[matriz[i - 1][j - 1] - 1], tem_row);
+        }
+      }
       if ((i + j) % 2 == 0 && matriz[i - 1][j - 1] == 0) {
       }
       else {
@@ -48,5 +64,5 @@ void display() {
     else
       table_chess = up(table_chess, tem_row);
   }
-  interpreter(pieces[7]);
+  interpreter(table_chess);
 }
